3b44f6b9d3bdb1a1fdc797350d8579544aa22fd3
Danilo Gomes <danilogomesdev@gmail.com>
2017-12-07 22:16:02 +0000

    loading dialog signin

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../../services/auth';
 
 
 @Component({
@@ -9,8 +10,31 @@ import { NgForm } from '@angular/forms';
 })
 export class SigninPage {
 
-  onSignin(form : NgForm){
 
-  }
+    constructor(private authService : AuthService,
+        private loadingController : LoadingController,
+        private alertController : AlertController){}
 
+    onSignin(form : NgForm){
+        const loadingDialog = this.loadingController.create({
+            content : 'Signing you in...'
+        });
+
+        loadingDialog.present();
+        this.authService.signin(form.value.email, form.value.password)
+        .then(data => {
+            loadingDialog.dismiss();
+            console.log(data);
+        }).catch(error => {
+            loadingDialog.dismiss();
+            console.log(error)
+
+            const alert = this.alertController.create({
+                title : 'Signin failed',
+                message : error.message,
+                buttons : [ 'Ok']
+            });
+            alert.present();
+        });
+      }
 }
