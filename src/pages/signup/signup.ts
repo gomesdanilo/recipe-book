58264d55a04d1fe005e33ea19ce38e8d2290d2a0
Danilo Gomes <danilogomesdev@gmail.com>
2017-12-07 22:11:25 +0000

    loading dialog signup

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth';
-
 
 @Component({
   selector: 'page-signup',
@@ -10,12 +9,30 @@ import { AuthService } from '../../services/auth';
 })
 export class SignupPage {
 
-  constructor(private authService : AuthService){}
+  constructor(private authService : AuthService,
+              private loadingController : LoadingController,
+              private alertController : AlertController){}
   
   onSignup(form : NgForm){
+    const loadingDialog = this.loadingController.create({
+      content : 'Signing you up...'
+    });
+
+    loadingDialog.present();
     this.authService.signup(form.value.email, form.value.password)
     .then(data => {
-        console.log(data);
-    }).catch(error => console.log(error));
+      loadingDialog.dismiss();
+      console.log(data);
+    }).catch(error => {
+      loadingDialog.dismiss();
+      console.log(error)
+
+      const alert = this.alertController.create({
+        title : 'Signup failed',
+        message : error.message,
+        buttons : [ 'Ok']
+      });
+      alert.present();
+    });
   }
 }
